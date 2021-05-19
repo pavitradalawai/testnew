8c94b060bb6d6342e3c6f69d59f3dbf857dd5175
pavitradalawai <83263125+pavitradalawai@users.noreply.github.com>
2021-05-19 12:48:25 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,16 @@
+class ArrayDemo{
+public static void main(String[]args){
+	int evenNumbers[]={2,4,6,8,10,11};
+
+System.out.println(evenNumbers[4]);
+
+System.out.println("Size of evenNumbers "+evenNumbers.length);
+	
+
+int[]OddNumbers=new int[10];
+System.out.println("Size of OddNumbers "+OddNumbers.length);
+System.out.println(OddNumbers[0]);
+OddNumbers[0]=10;
+System.out.println(OddNumbers[0]);
+}
+}
